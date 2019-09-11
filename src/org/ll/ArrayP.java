c4463726c46a65321497fe133ba0379d62b4a30b
bindugit <bindu.gadaparthi@gmail.com>
2019-09-11 17:15:44 +0530

    array

---
+++ src/org/ll/ArrayP.java
@@ -0,0 +1,18 @@
+package org.ll;
+
+import java.util.ArrayList;
+
+public class ArrayP {
+	public static void main(String[] args) {
+		ArrayList emp=new ArrayList();
+		emp.add("srr");
+		emp.add(123);
+		emp.add("lov");
+		System.out.println(emp);
+		emp.set(0, "syy");
+		System.out.println("After replace");
+		System.out.println(emp);
+		System.out.println("Size of the array:-" +emp.size());
+	}
+
+}
